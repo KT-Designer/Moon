095ef583aade158dadb2b039b6d660c2871c491c
Doris <130676335+KT-Designer@users.noreply.github.com>
2023-08-26 03:16:51 +0800

    Add files via upload

---
+++ js/swiper.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    autoplay: {
+        delay: 1500,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+    },
+
+});
